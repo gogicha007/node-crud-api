b16adc8a7c0c3d5f4cb1f6c438b86c6cf9cd26c1
gogicha007 <gogicha@gmail.com>
2024-10-20 22:58:39 +0400

    chore: save the progress

---
+++ src/loadBalancer.ts
@@ -0,0 +1,36 @@
+import cluster from 'node:cluster';
+import http, { ServerResponse, RequestOptions, createServer } from 'node:http'
+import os from 'node:os';
+import { Server } from './server';
+import url from 'node:url'
+
+let PORT = 4000;
+let numCPUs = os.availableParallelism();
+
+const workerPorts = Array.from({ length: numCPUs - 1 }, (_, i) => PORT + i + 1);
+
+
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+
+  // const roundRobin = (servers, req, res) => {
+  //   const target  = servers[current]
+  //   current = (current + 1) % (numCPUs-1)
+  // }
+  // workerPorts.forEach((workerPort) => {
+  //   console.log(workerPort)
+  //   const child = cluster.fork({PORT: workerPort});
+  //   child.on('message', async (message) => {
+  //     console.log(message);
+  //     //   await doRequest(dbReqOptions('POST'), message);
+  //   });
+  // });
+
+  cluster.on('exit', (worker) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
+} else {
+  new Server().server.listen(PORT, () => {
+    console.log(`Worker ${process.pid} started. Server running on port ${PORT}`);
+  });
+}
--- src/primary.ts
+++
@@ -1,7 +0,0 @@
-import cluster from 'node:cluster';
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import os from 'node:os';
-
-// const __dirname = dirname(fileURLToPath(import.meta.url))
-
